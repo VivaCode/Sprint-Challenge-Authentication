b92467ba5be6a7496c6a62bfdd4e6b613d6e37c6
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-10-26 10:55:49 -0500

    register, login functinoal. moving on to stretch.

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig');
+const {generateToken, authenticate} = require('./middlewares');
 
-const { authenticate } = require('./middlewares');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,10 +12,40 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+    const credentials = req.body;
+  
+    const hash = bcrypt.hashSync(credentials.password, 10);
+    credentials.password = hash;
+  
+    db('users')
+    .insert(credentials)
+    .then(ids => {
+        const id = ids[0];
+        res.status(201).json({ newUserId: id });
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
 }
 
 function login(req, res) {
   // implement user login
+  const creds = req.body;
+  
+    db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+        if (user && bcrypt.compareSync(creds.password, user.password)) {
+            const token = generateToken(user); 
+            res.status(200).json({ welcome: user.username, token });
+        } else {
+            res.status(401).json({ message: 'you shall not pass!' });
+        }
+    })
+    .catch(err => {
+    res.status(500).json({ err });
+    });
 }
 
 function getJokes(req, res) {
